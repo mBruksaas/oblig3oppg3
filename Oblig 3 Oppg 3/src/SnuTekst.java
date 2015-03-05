979437082d51817931a0b74a7805f5796c49c626
mBruksaas <martin_4144@hotmail.com>
2015-03-05 17:32:11 +0100

    v0.2

--- Oblig 3 Oppg 3/src/SnuTekst.java
+++ Oblig 3 Oppg 3/src/SnuTekst.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class SnuTekst {
 
+	static int a = 1;
+	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Skriv inn litt tekst: ");
@@ -10,15 +12,20 @@ public class SnuTekst {
 		
 		System.out.print("\"" + s + "\" baklengs er \"");
 		baklengs(s);
+		
+		/*Unødvendig komplisering av et ellers enkelt problem
+		 * System.out.println("Antall tegn: " + s.length());
+		 */
 	}
 	
 	public static void baklengs(String tekst) {
 		int l = tekst.length();
 		if(l != 1) {
+			a++;
 			System.out.print(tekst.charAt(l-1));
 			baklengs(tekst.substring(0, l-1));
 		} else {
-			System.out.println(tekst + "\"");
+			System.out.println(tekst + "\"\nAntall tegn: " + a);
 		}
 	}
 }
